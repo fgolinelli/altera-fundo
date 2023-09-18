2f519f85dd3ddc0df78c990847a86764fde0df59
fgolinelli <fernando@golinelli.eti.br>
2023-09-18 16:51:59 -0300

    Alterações finais 18/09/2023 16:51

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const botoes = document.getElementsByTagName("button");
 const texto = document.getElementById("cor-atual");
-console.log(texto.innerText);
 const cores = ["#D94E33","#2C5697","#ED9B33", "#8A9B8E", "#DDE5ED"];
 
 for (let i = 0; i < botoes.length; i++){
