a43704c7e62bfb073164dc76bf204237c0686d0a
fgolinelli <fernando@golinelli.eti.br>
2023-09-18 16:50:40 -0300

    Alterações 18/09/2023 16:50

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const botoes = document.getElementsByTagName("button");
 const texto = document.getElementById("cor-atual");
-document.getElementById("botao").style.backgroundColor = "#ED9B33";
+console.log(texto.innerText);
 const cores = ["#D94E33","#2C5697","#ED9B33", "#8A9B8E", "#DDE5ED"];
 
 for (let i = 0; i < botoes.length; i++){
